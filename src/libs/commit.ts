42b16a811489f6830f2b20607f7a3c8c0cea4377
Shad02w <alvistse123@gmail.com>
2021-09-09 14:31:21 +0800

    (Fix) Fixed exit code of the process

--- src/libs/commit.ts
+++ src/libs/commit.ts
@@ -1,6 +1,9 @@
 import { spawnSync } from 'child_process'
 
 export function commit(message: string) {
-    spawnSync('git', ['commit', '-m', JSON.stringify(message)], { shell: true, stdio: 'inherit' })
-    process.exit(1)
+    const result = spawnSync('git', ['commit', '-m', JSON.stringify(message)], { shell: true, stdio: 'inherit' })
+    if (result.status === null || result.status != 0) {
+        process.exit(1)
+    }
+    process.exit(0)
 }
