242213441434a6df13e48ba01f9a76fb4e00dcad
Shad02w <alvistse123@gmail.com>
2021-09-09 14:28:25 +0800

    (Refactor) exit process when commit ended

--- src/libs/commit.ts
+++ src/libs/commit.ts
@@ -1,8 +1,6 @@
 import { spawnSync } from 'child_process'
 
 export function commit(message: string) {
-    const result = spawnSync('git', ['commit', '-m', JSON.stringify(message)], { shell: true, stdio: 'inherit' })
-    if (result.status === null || result.status != 0) {
-        process.exit(1)
-    }
+    spawnSync('git', ['commit', '-m', JSON.stringify(message)], { shell: true, stdio: 'inherit' })
+    process.exit(1)
 }
