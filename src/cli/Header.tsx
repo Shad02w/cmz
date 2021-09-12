26760a8e1efc7206314dad8512a8f3631f0fe4ba
Shad02w@Win10 <alvistse123@gmail.com>
2021-09-12 23:06:18 +0800

    (Fix) Fixed the wrong version in Header

--- src/cli/Header.tsx
+++ src/cli/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import packageJson from '../../package.json'
 import { Text } from 'ink'
 
 export const Header = React.memo(() => {
-    return <Text>Commitz - v1.0.0</Text>
+    return <Text>Commitz - v{packageJson.version}</Text>
 })
