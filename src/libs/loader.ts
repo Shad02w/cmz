d1a4ec7a9d44b064061b760e8c2709329c2212a5
Shad02w <alvistse123@gmail.com>
2021-09-13 22:20:56 +0800

    (Refactor) Added TS config support

--- src/libs/loader.ts
+++ src/libs/loader.ts
@@ -1,13 +1,14 @@
 import fs from 'fs/promises'
 import Module from 'module'
 import { createContext, Script } from 'vm'
-import { getNearestFilePath } from '../utils/LoaderUtil'
+import { getNearestFilePath, requireTSFile, requireJSFile } from '../utils/LoaderUtil'
 import path from 'path'
-import ts from 'typescript'
 import type { Config } from './config'
 
 const LoaderUtil = {
-    resolveConfigFilePath: getNearestFilePath,
+    getNearestFilePath,
+    requireTSFile,
+    requireJSFile,
 }
 
 /**
@@ -20,27 +21,20 @@ function getConfigFileType(filePath: string): 'TS' | 'JS' | null {
     const ext = path.extname(filePath)
     return ext === '.ts' ? 'TS' : ext === '.js' ? 'JS' : null
 }
-
-async function transpileTsFile(filePath: string) {
-    const buffer = await fs.readFile(path.resolve(filePath), { encoding: 'utf-8' })
-    return ts.transpile(buffer, {
-        target: ts.ScriptTarget.ES2016,
-        module: ts.ModuleKind.CommonJS,
-        noEmit: true,
-        esModuleInterop: true,
-        strict: true,
-        moduleResolution: ts.ModuleResolutionKind.NodeJs,
-    })
-}
-
 async function loadJSConfig(filePath: string): Promise<Config> {
-    return require(filePath) as Config
+    return (await LoaderUtil.requireJSFile(filePath)) as Config
 }
 
 async function loadTSConfig(filePath: string): Promise<Config> {
-    const script = new Script(await transpileTsFile(filePath))
+    try {
+        require.resolve('ts-node')
+    } catch (error) {
+        throw new Error('Module `ts-node` is needed to load config in typescript')
+    }
+    const script = new Script(await LoaderUtil.requireTSFile(filePath))
     const req = Module.createRequire(filePath)
     const mo = new Module(filePath)
+
     const sandBox = {
         exports: mo.exports,
         module: mo,
@@ -55,7 +49,11 @@ async function loadTSConfig(filePath: string): Promise<Config> {
 }
 
 export async function loadConfig(): Promise<Config> {
-    const filePath = await LoaderUtil.resolveConfigFilePath(process.cwd(), `${CONFIG_FILE_NAME}.js`)
+    const filePath = await LoaderUtil.getNearestFilePath(process.cwd(), [
+        `${CONFIG_FILE_NAME}.ts`,
+        `${CONFIG_FILE_NAME}.js`,
+    ])
+
     if (!filePath) {
         throw new Error(`Unable to find config file. Please Add ${CONFIG_FILE_NAME} file to the workspace`)
     } else {
@@ -63,7 +61,7 @@ export async function loadConfig(): Promise<Config> {
         if (fileType === 'JS') {
             return loadJSConfig(filePath)
         } else if (fileType === 'TS') {
-            throw new Error('Config file in typescript currently is not supported')
+            return loadTSConfig(filePath)
         } else {
             throw new Error('Config file should be json, js and ts file')
         }
--- src/utils/LoaderUtil.ts
+++ src/utils/LoaderUtil.ts
@@ -1,19 +1,22 @@
 import path from 'path'
 import fs from 'fs/promises'
+import ts from 'typescript'
 
 /**
  * This will resolve the nearest files in workspace folder tree
  */
-export async function getNearestFilePath(startDirectory: string, fileName: string): Promise<string | null> {
+export async function getNearestFilePath(startDirectory: string, fileNames: string | string[]): Promise<string | null> {
     const parsed = path.parse(startDirectory)
     const pathClips = [...parsed.dir.replace(parsed.root, '').split(path.sep), parsed.base]
+    const files = typeof fileNames === 'string' ? [fileNames] : fileNames
 
     const searchPaths = pathClips
         .map((_, index) => {
             const clips = pathClips.slice(0, pathClips.length - index)
             return path.join(parsed.root, ...clips)
         })
-        .map(resolvedDirectory => path.join(resolvedDirectory, fileName))
+        .map(resolvedDirectory => files.map(_ => path.join(resolvedDirectory, _)))
+        .flatMap(_ => _)
         .filter(_ => path.isAbsolute(_))
 
     return await checkFileOrDirectoryExistence(searchPaths)
@@ -32,4 +35,28 @@ export async function checkFileOrDirectoryExistence(paths: string[]): Promise<st
         }
     }
     return null
+}
+
+export async function requireJSFile(filePath: string): Promise<any> {
+    return require(filePath)
+}
+
+export async function transpileTsFile(filePath: string): Promise<string> {
+    const buffer = await fs.readFile(path.resolve(filePath), { encoding: 'utf-8' })
+    return ts.transpile(buffer, {
+        target: ts.ScriptTarget.ES2016,
+        module: ts.ModuleKind.CommonJS,
+        noEmit: true,
+        esModuleInterop: true,
+        strict: true,
+        moduleResolution: ts.ModuleResolutionKind.NodeJs,
+    })
+}
+
+export async function requireTSFile(filePath: string) {
+    return `require('ts-node').register({
+        transpileOnly: true,
+    })
+    
+    ${await transpileTsFile(filePath)}`
 }
