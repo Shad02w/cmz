9cffd6df654965de4f65ccf0ff151252f692b0a8
Shad02w <alvistse123@gmail.com>
2021-09-28 18:14:32 +0800

    (Feature) Able to pass config file path as arg to the cli

--- src/bin.tsx
+++ src/bin.tsx
@@ -2,5 +2,17 @@
 import React from 'react'
 import { render } from 'ink'
 import { CLI } from './app'
+import { CMZCli } from './cli'
+import type { Options } from './cli'
+import { handle } from '@oclif/errors'
 
-render(<CLI />)
+async function main() {
+    try {
+        const { configPath } = (await CMZCli.run()) as Options
+        render(<CLI configPath={configPath} />)
+    } catch (error) {
+        handle(error as any)
+    }
+}
+
+main()
---
+++ src/cli.ts
@@ -0,0 +1,24 @@
+import { Command, flags } from '@oclif/command'
+
+export interface Options {
+    configPath?: string
+}
+
+export class CMZCli extends Command {
+    static description = 'A simple git commit tool'
+
+    static flags = {
+        version: flags.version({ char: 'v' }),
+        config: flags.string({ char: 'c' }),
+        help: flags.help({ char: 'h' }),
+    }
+
+    async run(): Promise<Options> {
+        const {
+            flags: { config },
+        } = this.parse(CMZCli)
+        return {
+            configPath: config,
+        }
+    }
+}
