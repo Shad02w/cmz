32b2335dadb412462c890091e48a295bb4ea943a
Shad02w@Win10 <alvistse123@gmail.com>
2021-10-04 22:11:58 +0800

    (Refactor) Removed ink-spinner

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import { Text } from 'ink'
-import Spinner from 'ink-spinner'
 
-interface Props {}
-
-export const Loading = (props: Props) => {
-    return (
-        <Text>
-            <Text color="green">
-                <Spinner type="dots6" />
-            </Text>
-            {' Loading config...'}
-        </Text>
-    )
+export const Loading = () => {
+    return <Text>Loading config...</Text>
 }
