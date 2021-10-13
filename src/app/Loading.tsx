5e763923b006f04bac3c3d41698172deddfbe6ce
Shad02w@wsl2 <alvistse123@gmail.com>
2021-10-13 22:29:21 +0800

    (Refactor) Added back ink-spinner to Loading component

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import Spinner from 'ink-spinner'
 import { Text } from 'ink'
 
 export const Loading = () => {
-    return <Text>Loading config...</Text>
+    return (
+        <Text>
+            <Text color="green">
+                <Spinner type="dots" />
+            </Text>
+            {' Loading config...'}
+        </Text>
+    )
 }
